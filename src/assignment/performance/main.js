ee9abb0560f5a06e4fef8df4ffb8fe59bd1dc325
Praga Hartyanto Prabowo <hartyanto@gmail.com>
2021-02-08 19:31:55 +0700

    menampilkan waktu terakhir diupdate

--- src/assignment/performance/main.js
+++ src/assignment/performance/main.js
@@ -15,6 +15,11 @@ const workerCreated = document.getElementById('worker-created');
 const taskCreated = document.getElementById('task-created');
 const taskDone = document.getElementById('task-done');
 const taskCancel = document.getElementById('task-cancel');
+const lastUpWc = document.getElementById('last-upd-wc');
+const lastUpTc = document.getElementById('last-upd-tc');
+const lastUpTd = document.getElementById('last-upd-td');
+const lastUpTcan = document.getElementById('last-upd-tcan');
+
 
 store$.subscribe(() => {
   const state = store$.getState();
@@ -39,7 +44,11 @@ btnTaskCancel.onclick = function() {
 
 function render(state) {
   workerCreated.innerHTML = state.worker.created;
+  lastUpWc.innerHTML = state.worker.lastUpdateCreate
   taskCreated.innerHTML = state.task.created;
+  lastUpTc.innerHTML = state.task.lastUpdateCreate;
   taskDone.innerHTML = state.task.done;
+  lastUpTd.innerHTML = state.task.lastUpdateDone;
   taskCancel.innerHTML = state.task.cancel;
+  lastUpTcan.innerHTML = state.task.lastUpdateCancel;
 }
